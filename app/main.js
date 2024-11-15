2f9e719cc3286b2fc7d22ae231edc2c209703df3
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-11-15 13:58:26 -0500

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,1 +1,47 @@
 import "./style.css";
+
+//get data
+//promise
+//show data
+
+async function getBookData(){
+    try {
+        //returns a promise 
+        const response = await fetch ('https://stephen-king-api.onrender.com/api/books');
+        //guard clause 
+        if (response.status != 200) {
+            throw new Error(response);
+        }else{
+            //convert promise to json
+            const data = await response.json();
+            console.log(data.data);
+            //this is unique to THIS API
+            data.data.forEach((agent)=>console.log(agent.Title));
+        }
+    } catch (error) {
+        alert("hey  I could not find that agent")
+    }
+};
+
+async function getShortsData(){
+    try {
+        //returns a promise 
+        const response = await fetch ('https://stephen-king-api.onrender.com/api/shorts');
+        //guard clause 
+        if (response.status != 200) {
+            throw new Error(response);
+        }else{
+            //convert promise to json
+            const data = await response.json();
+            console.log(data.data);
+            //this is unique to THIS API
+            data.data.forEach((agent)=>console.log(agent.title));
+        }
+    } catch (error) {
+        alert("hey  I could not find that agent")
+    }
+}
+
+getBookData();
+getShortsData();
+
